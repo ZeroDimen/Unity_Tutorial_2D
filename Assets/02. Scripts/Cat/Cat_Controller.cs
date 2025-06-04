5558a1eac902e14a7a27cb760976dd1210d869ad
ZeroDimen <rlaehdrjs951@naver.com>
2025-06-04 17:57:03 +0900

    Add AudioSource, UI
    2025.06.04
    -- SoundManager를 통한 오디오 출력
    -- Canvas를 통한 UI 제작 및 시작화면 구현

--- Assets/02. Scripts/Cat/Cat_Controller.cs
+++ Assets/02. Scripts/Cat/Cat_Controller.cs
@@ -1,29 +1,37 @@
 using UnityEngine;
+using Cat; // 사운드 메니저가 있는 namespace
 
 // 고양이 오브젝트를 이동 및 2단 점프 구현 코드
 public class Cat_Controller : MonoBehaviour
 {
+    public Cat_SoundManager soundManager; // public으로 설정 했기 때문에 유니티 상에서 할당 예정
+
     private Rigidbody2D Cat_Rb;
+    private Animator Cat_Anim;
     public float Cat_Speed;
     public float Cat_JumpForce;
     private float h;
     public int Cat_Jumpcount; // 현재 점프 수
-    
+
     private bool IsGround;
+
     void Start()
     {
         Cat_Rb = gameObject.GetComponent<Rigidbody2D>();
+        Cat_Anim = gameObject.GetComponent<Animator>();
     }
 
     void Update()
     {
         h = Input.GetAxis("Horizontal");
-        
+
         if (Input.GetKeyDown(KeyCode.Space) && Cat_Jumpcount < 2) // 스페이스 키 입력하면 실행, 연속 점프 기능
         {
             // AddForceY(Float 속도, Mode)
             Cat_Rb.AddForceY(Cat_JumpForce, ForceMode2D.Impulse);
             Cat_Jumpcount++;
+            Cat_Anim.SetBool("IsGround", false); // 점프 에니메이션 출력
+            soundManager.OnJumpSound();
         }
     }
 
@@ -37,8 +45,9 @@ public class Cat_Controller : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             Cat_Jumpcount = 0;
+            Cat_Anim.SetBool("IsGround", true); // 걷기 에니메이션 출력
 
         }
-        
+
     }
 }
---
+++ Assets/02. Scripts/Cat/Cat_SoundManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+// 유니티에서 소리를 출력, 설정하는 스크립트
+namespace  Cat
+{
+    public class Cat_SoundManager : MonoBehaviour
+    {
+        public AudioSource audioSource;
+        public AudioClip Bgm_Intro_Clip;
+        public AudioClip Bgm_Game_Clip;
+        public AudioClip Jump_Clip;
+
+        private void Start()
+        {
+            SetBGM_Intro_Sound();
+        }
+
+        public void SetBGM_Intro_Sound() // 인트로 배경음 출력함수
+        {
+            audioSource.clip = Bgm_Intro_Clip; // 오디오 소스에 사운드 파일 설정 
+            audioSource.playOnAwake = true; // 시작할 때 자동 재생
+            audioSource.loop = true; // 반복 기능
+            audioSource.volume = 0.5f; // 소리 음량
+            
+            audioSource.Play(); // 시작
+            // audioSource.Stop(); 정지 
+            // audioSource.Pause(); 일시정지
+        }
+
+        public void SetBGM_Game_Sound() // 게임 배경음 출력함수
+        {
+            audioSource.clip = Bgm_Game_Clip;
+            audioSource.Play(); // 시작
+            Debug.Log("VAR");
+        }
+        public void OnJumpSound() // 점프 효과음 출력함수
+        {
+            // PlayOneShot(AudioClip.name) 한번 출력, 임시로 사용, 제어 불가 (클립을 매개변수로 사용)
+            audioSource.PlayOneShot(Jump_Clip); // 설정 변경 불가 
+        }
+    }
+}
+
